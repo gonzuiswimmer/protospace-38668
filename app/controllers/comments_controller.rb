f1feb4146a69f174b49f6e97c12a13110f497b82
gonzuiswimmer <gonzuiswimmer@gmail.com>
2022-11-03 16:36:23 +0900

    コメントコントローラーの修正
    コメントが空で送信した場合にエラーが生じる点を解消しました

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,15 +1,14 @@
 class CommentsController < ApplicationController
 
   def create
-    @comment = Comment.create(comment_params)
+    @comment = Comment.new(comment_params)
     @prototype = Prototype.find(params[:prototype_id])
     if @comment.save
       redirect_to prototype_path(@prototype.id)
     else
       @prototype = @comment.prototype
-      # @comments = @prototype.comments
+      @comments = @prototype.comments
       render "prototypes/show"
-      # ↑実装できてるけど、理解がまだ
     end
   end
 
